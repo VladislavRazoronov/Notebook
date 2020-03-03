615bbeebb04375dc94b0afaa94d97635d4a9d0df
Vladislav Razoronov <razoronov@ucu.edu.ua>
2020-03-03 16:12:45 +0200

    update

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import datetime
 class Note:
+    """
+    Class for representing notes, their contents, dates
+    and tags
+    """
     def __init__(self,memo,tags=""):
         self.memo = memo
         self.creation_date = datetime.date.today()
         self.tags = tags
     def match(self,search_filter):
+        """
+        Checks if selected string is in note's
+        memo or tags
+        """
         if search_filter in self.memo:
             return True
         if search_filter in self.tags:
@@ -16,6 +24,18 @@ class Note:
         return f"Note({str(self.creation_date)},{self.memo},{self.tags})"
 
 class Notebook:
+    """
+    Class for representing notebook as a list
+    of notes with metods:
+    .new_note(memo,tags)- Adds a new note with 
+    given memo and tags to notebook
+    .modify_memo(note_id,memo)- Modifies memo of
+    selected note
+    .search(search_filter)- Finds a list of all
+    notes containing a given element
+    .modify_tags(note_id,tags)- Modifies tags of
+    selected note
+    """
     def __init__(self,notes=[]):
         self.notes = notes
     def search(self,search_filter):
@@ -25,10 +45,20 @@ class Notebook:
                 found.append(note)
         return found
     def new_note(self,memo,tags=""):
+        """
+        Creates a new note with given memo and tags
+        in notebook
+        """
         self.notes.append(Note(memo,tags))
     def modify_memo(self,note_id,memo):
+        """
+        Modifies memo of slected note
+        """
         self.notes[note_id].memo = memo
     def modify_tags(self,note_id,tags):
+        """
+        Modifies tags of selected note
+        """
         self.notes[note_id].tags = tags
     def __str__(self):
         notes = ""
